08acabf160d7e64317f215b5d9dc5127292307e6
Yamil28XD <yamil3307@gmail.com>
2025-09-08 14:37:54 -0600

    Actividad 4

--- Actividad4/Main.java
+++ Actividad4/Main.java
@@ -20,17 +20,17 @@ public class Main {
 
         // Precargar 100 empleados con nombres y trabajos aleatorios
         for (int i = 1; i <= 100; i++) {
-            String nomAleatorio = nombres[rand.nextInt(nombres.length)] + "_" + i;
+            String nomAleatorio = nombres[rand.nextInt(nombres.length)];
             String trabajoAleatorio = trabajos[rand.nextInt(trabajos.length)];
             arbolito.agregarNodo(i, nomAleatorio, trabajoAleatorio);
         }
 
         do {
             System.out.println("Menu");
-            System.out.println("1. Agregar un Nodo");
-            System.out.println("2. Recorrer el árbol Inorden");
-            System.out.println("3. Buscar un Empleado por ID");
-            System.out.println("4. Salir");
+            System.out.println("1) Agregar un Nodo");
+            System.out.println("2) Recorrer el árbol Inorden");
+            System.out.println("3) Buscar un Empleado por ID");
+            System.out.println("4) Salir");
             System.out.print("Elige una opción: ");
             opcion = sc.nextInt();
 
@@ -38,13 +38,17 @@ public class Main {
                 case 1:
                     System.out.print("Ingresa el número del nodo (ID): ");
                     elemento = sc.nextInt();
+                    if (arbolito.buscarNodo(elemento) != null || elemento<0) {
+                        System.out.println("El ID ya existe o es menor a 0");
+                    } else {
+                        System.err.println("ID agregagado");
                     sc.nextLine(); // limpiar buffer
                     System.out.print("Ingresa el nombre del empleado: ");
                     nombre = sc.nextLine();
                     System.out.print("Ingresa el trabajo del empleado: ");
                     trabajo = sc.nextLine();
                     arbolito.agregarNodo(elemento, nombre, trabajo);
-                    System.out.println("Empleado agregado correctamente.");
+                    }
                     break;
 
                 case 2:
